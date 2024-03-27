8abaa77c80f3073006d2200198642476a169f738
solo _belay <123241980+solo-belay@users.noreply.github.com>
2024-03-26 23:40:22 -0700

    App.js

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview';
-export default function App() {
-  return (
-   <WebView style={styles.container}
-   source={{uri:'https://megegna.com'}}
-   />
-  );
-}
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { BackHandler } from "react-native";
+import { WebView } from "react-native-webview";
 
+const App = () => {
+  const webviewRef = useRef(null);
+
+  useEffect(() => {
+    const handleBackPress = () => {
+      if (webviewRef.current) {
+        if (webviewRef.current.canGoBack()) {
+          webviewRef.current.goBack();
+          return true; // Indicate that we've handled the back press
+        }
+      }
+      return false; // Let the default behavior happen
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleBackPress
+    );
+
+    return () => backHandler.remove(); // Clean up event listener on unmount
+  }, []);
+
+  const handleSwipeDown = () => {
+    if (webviewRef.current) {
+      webviewRef.current.reload();
+    }
+  };
+
+  return (
+    <WebView
+      ref={webviewRef}
+      source={{ uri: "http://www.megegna.com/" }}
+      onSwipeDown={handleSwipeDown}
+    />
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+export default App;
